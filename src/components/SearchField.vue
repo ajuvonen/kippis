14c2802889b52e0401b1965a7ce525eda71d7daa
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-08-01 09:16:03 +0300

    Add translations

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -2,14 +2,16 @@
 import {ref} from 'vue';
 import {useRouter} from 'vue-router';
 import {ALCOHOLS} from '@/utils/constants';
+import {useI18n} from 'vue-i18n';
 
 const router = useRouter();
+const {t} = useI18n();
 
 const searchString = ref('');
 </script>
 
 <template>
-  <label for="search-box" class="text-3xl mb-4">Search for Recipes</label>
+  <label for="search-box" class="text-3xl mb-4">{{ t('searchField.label') }}</label>
   <div class="flex justify-center divide-x mb-4">
     <input
       id="search-box"
@@ -18,12 +20,26 @@ const searchString = ref('');
       maxlength="20"
       class="rounded-tr-none rounded-br-none pb-1"
     />
-    <button class="rounded-tl-none rounded-bl-none" @click="router.push({name: 'search', query: {searchString}})">
-      Search
+    <button
+      class="rounded-tl-none rounded-bl-none"
+      @click="router.push({name: 'search', query: {searchString}})"
+    >
+      {{ t('searchField.search') }}
     </button>
   </div>
   <div class="flex flex-wrap justify-center">
-    <button v-for="{tag} in ALCOHOLS" :key="tag" @click="router.push({name: 'search', query: {tag}})">{{ tag }}</button>
-    <button>I'm Feeling Lucky</button>
+    <ul :aria-label="t('searchField.tagListTitle')">
+      <li class="inline" v-for="{tag} in ALCOHOLS" :key="tag">
+        <button @click="router.push({name: 'search', query: {tag}})">
+          {{ t(`tags.${tag}`) }}
+        </button>
+      </li>
+    </ul>
+    <button clasS="ml-1">{{ t('searchField.random') }}</button>
   </div>
 </template>
+<style scoped>
+li + li {
+  @apply ml-1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
 import {onMounted, ref} from 'vue';
+import {useI18n} from 'vue-i18n';
 import SearchField from '@/components/SearchField.vue';
 import {MAIN_PHOTOS} from '@/utils/constants';
 import type {MainPhoto} from '@/utils/types';
+
+const {t} = useI18n();
 
 const mainPhoto = ref<MainPhoto>();
 onMounted(() => {
   const randomIndex = Math.floor(Math.random() * 3);
   mainPhoto.value = MAIN_PHOTOS[randomIndex];
 });
-
 </script>
 <template>
   <div class="flex flex-col sm:flex-row h-screen">
     <aside
       :style="{'background-image': `url('/${mainPhoto?.file}')`}"
-      class="h-1/2 sm:h-full sm:w-1/2 bg-slate-800 text-slate-100 text-center bg-cover bg-center">
-      <div class="flex flex-col p-4 justify-center h-full bg-gradient-to-b from-transparent to-slate-800 to-80%">
-        <h1>Enter a world of delicious cocktails!</h1>
-        <p>Use this service to find inspirational cocktails, plan your own cocktail party, and create a shopping list for a memorable evening. Use the search box or ingredient tags to get started.</p>
+      class="h-1/2 sm:h-full sm:w-1/2 bg-slate-800 text-slate-100 text-center bg-cover bg-center"
+    >
+      <div
+        class="flex flex-col p-4 justify-center h-full bg-gradient-to-b from-transparent to-slate-800 to-80%"
+      >
+        <h1>{{ t('homeView.title') }}</h1>
+        <p>{{ t('homeView.ingress') }}</p>
       </div>
     </aside>
     <main class="h-1/2 sm:h-full sm:w-1/2">
@@ -26,5 +31,4 @@ onMounted(() => {
     </main>
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
