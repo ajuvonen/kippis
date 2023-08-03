55217dcfc24c9c877be49fe9b13caf4fcecf4d64
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-08-03 16:37:46 +0300

    Add search result hover animation

--- src/components/SearchResultCard.vue
+++ src/components/SearchResultCard.vue
@@ -6,29 +6,40 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div
-    :style="{background: `url(${item.thumb})`, backgroundSize: 'cover'}"
-    class="search-result__image"
-    role="button"
-    tabindex="0"
-  >
-    <div
-      class="search-result__shadow"
-    >
-      {{ item.name }}
+  <div class="search-result__wrapper">
+    <div class="search-result__image-wrapper" role="button" tabindex="0">
+      <div
+        :style="{background: `url(${item.thumb})`, backgroundSize: 'cover'}"
+        class="search-result__image"
+      ></div>
+      <div class="search-result__shadow">
+        {{ item.name }}
+      </div>
     </div>
     <button class="search-result__add-button">Add</button>
   </div>
 </template>
 <style lang="scss" scoped>
-.search-result__image {
-  filter: contrast(115%);
+.search-result__wrapper {
+  @apply relative shadow-md rounded-md hover:cursor-pointer hover:shadow-lg transition-shadow ease-in-out;
+}
+
+.search-result__image-wrapper {
+  @apply relative overflow-hidden w-40 h-40 rounded-md;
   outline-offset: 0.2rem;
-  @apply hover:cursor-pointer hover:shadow-rose-300 transition-shadow ease-in-out relative w-40 h-40 rounded-md shadow-lg;
+}
+
+.search-result__image {
+  @apply h-full;
+  filter: contrast(115%);
+  transition: transform 0.5s ease-in-out;
+  &:hover {
+    transform: scale(1.1) rotate(-5deg);
+  }
 }
 
 .search-result__shadow {
-  @apply w-full h-full bg-gradient-to-b from-slate-800 to-transparent to-40% rounded-md text-white uppercase text-xs p-2;
+  @apply absolute w-full h-full top-0 pointer-events-none bg-gradient-to-b from-slate-800 to-transparent to-40% text-white uppercase text-xs p-2;
 }
 
 .search-result__add-button {
