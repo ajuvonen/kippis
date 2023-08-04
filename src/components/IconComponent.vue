82e45970b879f0f154975c6606097e61a44477e5
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-08-04 13:40:14 +0300

    Add mdi icon component

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+}>();
+</script>
+<template>
+  <i class="mdi" :class="`mdi-${icon}`" aria-hidden="true" />
+</template>
+<style lang="scss" scoped>
+.mdi {
+  @apply text-lg;
+}
+</style>
