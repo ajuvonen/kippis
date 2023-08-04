a72196c539cda4a75bafcb40325294e8c6eb394f
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-08-04 15:40:32 +0300

    Implement a result blacklist

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -18,6 +18,46 @@ export const ALCOHOLS = [
   {tag: 'virgin'},
 ];
 
+// TODO: oz, qt, fifth, gal, jigger
+
+// Drinks that are weird or have very poor images/instructions
+export const BLACKLIST = [
+  12790, // Absinthe #2
+  15224, // Malibu twister
+  15254, // Zenmeister
+  15288, // 252
+  15330, // Orange Crush
+  15346, // 155 Belmont
+  15409, // Danbooka
+  15423, // 110 in the Shade
+  15567, // Adam Sunrise
+  15691, // Zoksel
+  15743, // Fuzzy Asshole
+  15761, // Quick-sand
+  15789, // Snakebite and Black
+  15801, // Zimadori Zinger
+  15813, // Herbal Flame
+  15933, // Zambeer
+  16047, // Campari Beer
+  16178, // Jitterbug
+  16196, // Moranquito
+  16273, // Shark Attack
+  16403, // Black and Brown
+  16963, // Zorbatini
+  16985, // Shot-gun
+  16992, // Pink Penocha
+  17020, // Auburn Headbanger
+  17027, // Zima Blaster
+  17035, // Buccaneer
+  17044, // Homemade Kahlua
+  17105, // 501 Blue
+  17108, // Coke and Drops
+  17120, // Brain Fart
+  17122, // Royal Bitch
+  17167, // Raspberry Cooler
+  17174, // Cherry Electric Lemonade
+];
+
 export const BASE_API_ADDRESS = 'https://www.thecocktaildb.com/api/json/v1/1';
 
 export const MAIN_PHOTOS = [
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,19 +1,29 @@
-import type {SearchResultAPIDrink, FullDetailsAPIDrink, SearchResultDrink, FullDetailsDrink} from './types';
+import type {
+  SearchResultAPIDrink,
+  FullDetailsAPIDrink,
+  SearchResultDrink,
+  FullDetailsDrink,
+} from '@/utils/types';
+import {BLACKLIST} from '@/utils/constants';
 
 export const transformSearchResults = (drinks: SearchResultAPIDrink[]) =>
-  drinks.map<SearchResultDrink>(({idDrink, strDrink, strDrinkThumb}) => ({
-    id: +idDrink,
-    name: strDrink,
-    thumb: strDrinkThumb,
-  }));
+  drinks
+    .filter(({idDrink}) => !BLACKLIST.includes(+idDrink))
+    .map<SearchResultDrink>(({idDrink, strDrink, strDrinkThumb}) => ({
+      id: +idDrink,
+      name: strDrink,
+      thumb: strDrinkThumb,
+    }));
 
 export const transformFullDetails = (drinks: FullDetailsAPIDrink[]) =>
-  drinks.map<FullDetailsDrink>((drink) => ({
-    id: +drink.idDrink,
-    name: drink.strDrink,
-    thumb: drink.strDrinkThumb,
-    alcoholic: drink.strAlcoholic.toLowerCase() === 'alcoholic',
-    glass: drink.strGlass,
-    instructions: drink.strInstructions,
-    ingredients: []
-  }));
+  drinks
+    .filter(({idDrink}) => !BLACKLIST.includes(+idDrink))
+    .map<FullDetailsDrink>((drink) => ({
+      id: +drink.idDrink,
+      name: drink.strDrink,
+      thumb: drink.strDrinkThumb,
+      alcoholic: drink.strAlcoholic.toLowerCase() === 'alcoholic',
+      glass: drink.strGlass,
+      instructions: drink.strInstructions,
+      ingredients: [],
+    }));
