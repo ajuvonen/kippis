1ada913b4d3ff29d9696a7b9c4137f1843f6c59c
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-12-05 11:05:53 +0200

    Fix menu styles

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -10,14 +10,14 @@ const {selection} = storeToRefs(useCocktailStore());
 <template>
   <nav class="navigation-menu">
     <ul class="navigation-menu__link-container">
-      <li class="navigation-menu__link">
-        <RouterLink to="/">{{ t('menu.home') }}</RouterLink>
+      <li>
+        <RouterLink class="navigation-menu__link" to="/">{{ t('menu.home') }}</RouterLink>
       </li>
-      <li class="navigation-menu__link">
-        <RouterLink to="/search">{{ t('menu.search') }}</RouterLink>
+      <li>
+        <RouterLink class="navigation-menu__link" to="/search">{{ t('menu.search') }}</RouterLink>
       </li>
-      <li class="navigation-menu__link relative">
-        <RouterLink to="/selection">
+      <li>
+        <RouterLink class="navigation-menu__link relative" to="/selection">
           {{ t('menu.selection') }}
           <div v-if="selection.length" class="navigation-menu__selection-size">
             {{ selection.length }}
@@ -34,11 +34,12 @@ const {selection} = storeToRefs(useCocktailStore());
 
 .navigation-menu__link-container {
   @apply h-full flex justify-center;
+
+  > li {
+    @apply flex;
+  }
 }
 
-.navigation-menu__selection-size {
-  @apply absolute top-0 right-0 w-5 h-5 pt-[0.1rem] text-slate-800 font-bold text-center bg-rose-400 text-xs rounded-full;
-}
 
 .navigation-menu__link {
   @apply flex flex-col justify-center w-1/3 sm:w-28 text-center m-1 rounded-md uppercase text-sm tracking-wide transition-colors ease-in-out;
@@ -46,10 +47,14 @@ const {selection} = storeToRefs(useCocktailStore());
   &:hover {
     @apply bg-slate-800 text-white;
   }
+}
 
-  &.router-link-exact-active:not(:hover) {
-    @apply bg-slate-100;
-  }
+.router-link-exact-active:not(:hover) {
+  @apply bg-slate-100;
+}
+
+.navigation-menu__selection-size {
+  @apply absolute top-0 right-0 w-5 h-5 pt-[0.1rem] text-slate-800 font-bold text-center bg-rose-400 text-xs rounded-full;
 }
 
 @media print {
