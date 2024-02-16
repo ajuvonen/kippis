4d8cf72867da6bc18aa8e217951c1fe19d249a83
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-02-16 10:45:22 +0200

    Set document lang on locale change

--- src/components/LocaleChanger.vue
+++ src/components/LocaleChanger.vue
@@ -3,19 +3,19 @@ import {computed, ref, watch} from 'vue';
 import {useI18n} from 'vue-i18n';
 import IconComponent from '@/components/IconComponent.vue';
 
-const {availableLocales, locale} = useI18n();
+const {locale} = useI18n();
 
-const nextLocale = computed<string>(() => availableLocales.find((loc) => loc !== locale.value)!);
+const nextLocale = computed(() => locale.value === 'en' ? 'fi' : 'en');
 
 const changer = ref<HTMLButtonElement | null>(null);
 
-watch(locale, async () => {
-  changer.value?.focus();
+watch(locale, (newValue) => {
+  document.documentElement.lang = newValue;
 });
 </script>
 <template>
   <button
-    class="locale-changer left-button"
+    class="locale-changer left-button top-4"
     ref="changer"
     :aria-label="$t('localeChanger.changeLocale', [nextLocale])"
     @click="() => (locale = nextLocale)"
@@ -25,7 +25,4 @@ watch(locale, async () => {
   </button>
 </template>
 <style lang="scss" scoped>
-.locale-changer {
-  @apply top-4;
-}
 </style>
