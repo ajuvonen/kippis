b47f2b55ca217b608ca29f8e3e291beb1f82ddf5
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-10-10 12:43:32 +0300

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
