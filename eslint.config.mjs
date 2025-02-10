5ae932c35258755fd2a838bb2826bb9504e8659a
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-02-10 09:52:57 +0200

    Update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,18 @@
 import pluginVue from 'eslint-plugin-vue';
-import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import {defineConfigWithVueTs, vueTsConfigs} from '@vue/eslint-config-typescript';
 import pluginVitest from '@vitest/eslint-plugin';
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 import {includeIgnoreFile} from '@eslint/compat';
 import pluginVueA11y from 'eslint-plugin-vuejs-accessibility';
 import {fileURLToPath} from 'node:url';
 
-export default [
+export default defineConfigWithVueTs(
   includeIgnoreFile(fileURLToPath(new URL('./.gitignore', import.meta.url))),
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-  ...pluginVueA11y.configs['flat/recommended'],
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
+  {name: 'app/files-to-lint', files: ['**/*.{ts,mts,tsx,vue}']},
+  pluginVueA11y.configs['flat/recommended'],
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  {...pluginVitest.configs.recommended, files: ['src/**/__tests__/*']},
   skipFormatting,
-  {
-    rules: {
-      'vuejs-accessibility/label-has-for': [
-        'error',
-        {
-          required: {
-            some: ['nesting', 'id'],
-          },
-        },
-      ],
-    },
-  },
-];
+  {rules: {'vuejs-accessibility/label-has-for': ['error', {required: {some: ['nesting', 'id']}}]}},
+);
