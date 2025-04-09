0685eec829209646c08a4f082ac59ea48b09d461
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-04-09 08:42:51 +0300

    Include only Latin font variant

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from '@/App.vue';
 import router from '@/router';
 import i18n from '@/i18n';
 
-import '@fontsource/ephesis';
+import '@fontsource/ephesis/latin-400.css';
 import '@/assets/main.css';
 
 const app = createApp(App);
